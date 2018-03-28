b4b036276b44f0ee1620c5126fef55a13d1e4e28
nicolaasuni <nicola.asuni@genomicsplc.com>
2018-03-28 18:20:35 +0100

    Improve Python example

--- python/tests/example.py
+++ python/tests/example.py
@@ -1,13 +1,25 @@
-import variantkey as vh
+import variantkey as vk
 
-vk = vh.variantkey("X", 193330, "TCA", "TGTCG")
-print(vk)
+vkey = vk.variantkey("X", 193330, "GCA", "G")
+print(vkey)
+# 13259012477095908224
 
-s = vh.variantkey_string(vk)
+s = vk.variantkey_string(vkey)
 print(s)
+# b'b801799941c61380'
 
-v = vh.parse_variantkey_string(s)
+v = vk.parse_variantkey_string(s)
 print(v)
+# 13259012477095908224
 
-chrom, pos, refalt = vh.decode_variantkey(v)
+chrom, pos, refalt = vk.decode_variantkey(v)
 print(chrom, pos, refalt)
+# 23 193330 1103500160
+
+c = vk.decode_chrom(chrom)
+print(c)
+# X
+
+ref, alt, reflen, altlen = vk.decode_refalt(refalt)
+print(ref, alt, reflen, altlen)
+# GCA G 3 1
