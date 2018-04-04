443997d0e69c068c540dc3979ff9e79ff677fc80
nicolaasuni <nicola.asuni@genomicsplc.com>
2018-04-04 11:22:21 +0100

    New python function to reverse VariantKey

--- python/setup.py
+++ python/setup.py
@@ -27,7 +27,7 @@ class RunTests(Command):
 
 setup(
     name='variantkey',
-    version='2.1.2',
+    version='2.2.0',
     keywords=('variantkey variant key genetic genomics'),
     description="VariantKey Bindings for Python",
     long_description=read('../README.md'),
