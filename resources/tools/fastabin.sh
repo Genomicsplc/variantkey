cf5efcd0c24c2a1ef8a3d663aa429f16dd9fd6ee
nicolaasuni <nicola.asuni@genomicsplc.com>
2018-05-30 14:37:12 +0100

    Fix pipe fail

--- resources/tools/fastabin.sh
+++ resources/tools/fastabin.sh
@@ -3,15 +3,14 @@
 # fastabin.sh
 #
 # Create a binary version of the input genome sequence FASTA file for quick lookup.
-# It only extract sequences for chromosomes 1 to 22, X, Y and MT.
+# It only extract the first 25 sequences for chromosomes 1 to 22, X, Y and MT.
 #
-# BINARY FORMAT:
+# OUTPUT BINARY FORMAT:
 #
-# The first line of the binary fasta file contains the index.
-# The index is composed by 26 blocks of 32 bit numbers,
-# one for each of the 25 chromosomes (1 to 22, X, Y and MT),
+# The first line of the binary fasta file contains the index composed by 26 blocks
+# of 32 bit numbers, one for each of the 25 chromosomes (1 to 22, X, Y and MT),
 # plus one to indicate the end of the file.
-# Each index number represents the file byte offset of the corresponding sequence.
+# Each index number represents the file byte offset of the corresponding chromosome sequence.
 # The index is followed by 25 lines, one for each chromosome sequence.
 #
 #
@@ -22,23 +21,32 @@ set -e -u -x -o pipefail
 : ${REFERENCE_GENOME_FASTA_FILE:?}  # Input genome reference genome FASTA file
 : ${FASTA_BINARY_FILE:=fasta.bin}            # Name of the output binary FASTA file
 
-# add padding for the index section (26 x 4 bytes)
-echo $(printf '0000%.0s' {1..26}) > fasta.linear
+# linearize the fasta file : one line per sequence
+gawk '/^>/ {printf("%s",(N>0?"\n":""));N++;next;} {printf("%s",$0);} END {printf("\n");}' ${REFERENCE_GENOME_FASTA_FILE} > fasta_full_linear.tmp
 
-# linearize the fasta file : one line per sequence for the first 25 chromosomes
-gawk '/^>/ {printf("%s",(N>0?"\n":""));N++;next;} {printf("%s",$0);} END {printf("\n");}' ${REFERENCE_GENOME_FASTA_FILE} | head -n 25 >> fasta.linear || true
+# initialize file with padding for the index section (26 x 4 bytes)
+echo $(printf '0000%.0s' {1..26}) > fasta_linear.tmp
+
+# only keep the first 25 sequences
+head --quiet --lines 25 fasta_full_linear.tmp >> fasta_linear.tmp
+
+# remove temporary file
+rm -f fasta_full_linear.tmp
 
 # create the index in hexadecimal format
-gawk '{printf "%08x\n", (total += length($0)+1);}' fasta.linear > fasta.index.hex
+gawk '{printf "%08x\n", (total += length($0)+1);}' fasta_linear.tmp > fasta_index.tmp
 
 # convert HEX file in binary format
-xxd -r -p fasta.index.hex > ${FASTA_BINARY_FILE}
+xxd -r -p fasta_index.tmp > ${FASTA_BINARY_FILE}
+
+# remove temporary file
+rm -f fasta_index.tmp
 
 # append newline
 echo "" >> ${FASTA_BINARY_FILE}
 
 # merge files
-tail -n +2 fasta.linear >> ${FASTA_BINARY_FILE}
+tail -n +2 fasta_linear.tmp >> ${FASTA_BINARY_FILE}
 
-# remove temporary files
-rm -f fasta.index.hex fasta.linear
+# remove temporary file
+rm -f fasta_linear.tmp
