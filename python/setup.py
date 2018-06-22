bdaf812fbb57dabcc80a01b094939a300eb84e17
nicolaasuni <nicola.asuni@genomicsplc.com>
2018-06-22 16:25:38 +0100

    Test all "c" files for C++ compatibility

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name='variantkey',
-    version='2.6.7',
+    version='2.6.8',
     keywords=('variantkey variant key genetic genomics'),
     description="VariantKey Bindings for Python",
     long_description=read('../README.md'),
