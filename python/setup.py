ec6053eeb61ad4d433c5d8c6ad395d719058cd73
nicolaasuni <nicola.asuni@genomicsplc.com>
2018-06-27 09:32:09 +0100

    Update version in metadata

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name='variantkey',
-    version='2.6.8',
+    version='2.6.9',
     keywords=('variantkey variant key genetic genomics'),
     description="VariantKey Bindings for Python",
     long_description=read('../README.md'),
