f6a1b521bcd1d7ca5569ef390f01b0876b62d843
nicolaasuni <nicola.asuni@genomicsplc.com>
2018-07-27 20:16:28 +0100

    Complete R example

--- r/example/example.R
+++ r/example/example.R
@@ -88,6 +88,48 @@ print(x)
 # GENOREF FUNCTIONS
 # -----------------
 
+# Load the reference genome binary file.
+# The input reference binary files can be generated from a FASTA file using the resources/tools/fastabin.sh script.
+# This example uses the "c/test/data/genoref.bin".
+genoref <- MmapBinfile("../c/test/data/genoref.bin")
+idx <- LoadGenorefIndex(genoref$SRC)
+
+x <- GetGenorefSeq(genoref$SRC, idx, chrom=23, pos=0)
+print(x)
+# [1] 65
+
+x <- CheckReference(genoref$SRC, idx, chrom=23, pos=0, ref="A")
+print(x)
+# [1] 0
+
+x <- FlipAllele("ATCGMKRYBVDHWSNatcgmkrybvdhwsn")
+print(x)
+# [1] "TAGCKMYRVBHDWSNTAGCKMYRVBHDWSN"
+
+# Normalize a variant - this function should be used before generating a new VariantKey
+x <- NormalizeVariant(genoref$SRC, idx, chrom=13, pos=2, ref="CDE", alt="CFE")
+print(x)
+# $RET
+# [1] 48
+#
+# $POS
+# [1] 3
+#
+# $REF
+# [1] "D"
+#
+# $ALT
+# [1] "F"
+#
+# $REF_LEN
+# [1] 1
+#
+# $ALT_LEN
+# [1] 1
+
+MunmapBinfile(genoref$SRC, genoref$FD, genoref$SIZE)
+# [1] 0
+
 
 # /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 
@@ -95,6 +137,55 @@ print(x)
 # NRVK
 # ----
 
+# Load the lookup table for non-reversible variantkeys.
+# The input binary files can be generated from a normalized VCF file using the resources/tools/vkhexbin.sh script.
+# The VCF file can be normalized using the `resources/tools/vcfnorm.sh` script.
+# This example uses the "c/test/data/vknr.10.bin".
+vknr <- MmapBinfile("../c/test/data/vknr.10.bin")
+
+x <- FindRefAltByVariantkey(vknr$SRC, vknr$LAST, vk="2000c3521f1c15ab")
+print(x)
+# $REF
+# [1] "ACGTACGT"
+#
+# $ALT
+# [1] "ACGT"
+#
+# $REF_LEN
+# [1] 8
+#
+# $ALT_LEN
+# [1] 4
+#
+# $LEN
+# [1] 12
+
+# Reverse all VariantKeys, including the ones that are not directly reversible by using a lookup table.
+x <- ReverseVariantkey(vknr$SRC, vknr$LAST, vk="2000c3521f1c15ab")
+print(x)
+# $CHROM
+# [1] "4"
+#
+# $POS
+# [1] 100004
+#
+# $REF
+# [1] "ACGTACGT"
+#
+# $ALT
+# [1] "ACGT"
+#
+# $REF_LEN
+# [1] 8
+#
+# $ALT_LEN
+# [1] 4
+#
+# $LEN
+# [1] 12
+
+MunmapBinfile(vknr$SRC, vknr$FD, vknr$SIZE)
+# [1] 0
 
 # /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 
@@ -102,7 +193,84 @@ print(x)
 # RSIDVAR
 # -------
 
+# Load the lookup table for rsID to VariantKey.
+# The input binary file can be generated using the resources/tools/vkhexbin.sh script.
+# This example uses the "c/test/data/rsvk.10.bin".
+rsvk <- MmapBinfile("../c/test/data/rsvk.10.bin")
+
+x <- GetRvVariantkey(rsvk$SRC, item=3)
+print(x)
+# [1] "80010274003a0000"
+
+x <- FindRvVariantkeyByRsid(rsvk$SRC, 0, 9, rsid=0x00000061)
+print(x)
+# $VK
+# [1] "80010274003a0000"
+#
+# $FIRST
+# [1] 3
+
+x <- GetNextRvVariantkeyByRsid(rsvk$SRC, 2, 9, rsid=0x00000061)
+print(x)
+# $VK
+# [1] "80010274003a0000"
+#
+# $POS
+# [1] 3
+
+MunmapBinfile(rsvk$SRC, rsvk$FD, rsvk$SIZE)
+# [1] 0
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+# Load the lookup table for rsID to VariantKey.
+# The input binary file can be generated using the resources/tools/vkhexbin.sh script.
+# This example uses the "c/test/data/rsvk.m.10.bin".
+rsvkm <- MmapBinfile("../c/test/data/rsvk.m.10.bin")
+
+x <- FindAllRvVariantkeyByRsid(rsvkm$SRC, 0, 9, rsid=0x00000003)
+print(x)
+# [[1]]
+# [1] "80010274003a0000"
+#
+# [[2]]
+# [1] "8001028d00138000"
+#
+# [[3]]
+# [1] "80010299007a0000"
+
+MunmapBinfile(rsvkm$SRC, rsvkm$FD, rsvkm$SIZE)
+# [1] 0
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Load the lookup table for VariantKey ro rsID
+# The input binary file can be generated using the resources/tools/vkhexbin.sh script.
+# This example uses the "c/test/data/vkrs.10.bin".
+vkrs <- MmapBinfile("../c/test/data/vkrs.10.bin")
+
+x <- GetVrRsid(vkrs$SRC, item=3)
+print(x)
+# [1] 97
+
+x <- FindVrRsidByVariantkey(vkrs$SRC, 0, 9, vk="80010274003A0000")
+print(x)
+# $RSID
+# [1] 97
+#
+# $FIRST
+# [1] 3
+
+x <- FindVrChromposRange(vkrs$SRC, 0, 9, chrom=0x14, pos_min=0x000256C5, pos_max=0x000256CB)
+print(x)
+#$RSID
+#[1] 9973
+#
+#$FIRST
+#[1] 7
+#
+#$LAST
+#[1] 8
+
+MunmapBinfile(vkrs$SRC, vkrs$FD, vkrs$SIZE)
+# [1] 0
