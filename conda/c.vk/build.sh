0fa184b5d6c0677ff928b319a7c32b8c2d41b149
nicolaasuni <nicola.asuni@genomicsplc.com>
2018-09-21 16:42:05 +0100

    Add docs to vk package

--- conda/c.vk/build.sh
+++ conda/c.vk/build.sh
@@ -1,3 +1,10 @@
 echo -e "\n\n*** BUILD ***\n\n"
-mkdir -p "${PREFIX}/bin"
-cp "${RECIPE_DIR}/../../c/target/build/vk/vk" "${PREFIX}/bin/"
+BINDIR="${PREFIX}/bin"
+mkdir -p ${BINDIR}
+cp "${RECIPE_DIR}/../../c/target/build/vk/vk" ${BINDIR}
+DOCDIR="${PREFIX}/share/doc/vk/"
+mkdir -p ${DOCDIR}
+cp "${RECIPE_DIR}/../../README.md" ${DOCDIR}
+cp "${RECIPE_DIR}/../../RELEASE" ${DOCDIR}
+cp "${RECIPE_DIR}/../../VERSION" ${DOCDIR}
+cp "${RECIPE_DIR}/../../LICENSE" ${DOCDIR}
