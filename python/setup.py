85378e9aaf10847746ae11a0f953a79e11b1c4af
nicolaasuni <nicola.asuni@genomicsplc.com>
2018-09-21 16:30:53 +0100

    Bump version

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name='variantkey',
-    version='4.2.3',
+    version='4.2.4',
     keywords=('variantkey variant key genetic genomics'),
     description="VariantKey Bindings for Python",
     long_description=read('../README.md'),
